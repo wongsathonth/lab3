35077a2d642caeb0cd4b8cb7650aaebf2b61b29f
wongsathonth <119285369+wongsathonth@users.noreply.github.com>
2023-01-15 22:48:01 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        List<Student> Studentlist = new List<Student>();
+        List<double> GradeList = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = this.textBox1.Text;
+                string student_id = this.textBox4.Text;
+                string birthyear = this.textBox3.Text;
+                string height = this.textBox2.Text;
+                double grade = double.Parse(this.textBox5.Text);
+                string branch = this.textBox6.Text;
+
+                Student student = new Student(name, student_id, birthyear, height, grade, branch);
+                Studentlist.Add(student);
+
+                BindingSource source = new BindingSource();
+                source.DataSource = Studentlist;
+                this.dataGridView1.DataSource= source; 
+
+                foreach(var val in Studentlist)
+                {
+                    this.GradeList.Add(val.Grade);
+                }
+                double maxgrade=GradeList.Max();
+                double mingrade=GradeList.Min();
+
+                int allperson=Studentlist.Count();
+
+                this.label11.Text=allperson.ToString();
+                this.label9.Text=maxgrade.ToString();
+                this.label7.Text = mingrade.ToString();
+            }
+            catch {
+                return;
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace WindowsFormsApp1
+{
+    internal class Student
+    {
+        public string Name
+        { get; set; }
+        public string Student_id
+        { get; set; }
+
+        public string Birthyear
+        {
+            get; set;
+        }
+        public string Height
+        {
+            get; set;
+        }
+        public double Grade
+        {
+            get; set;
+        }
+        public string Branch
+        {
+            get; set;
+        }
+        public Student(string name, string student_id, string birthyear, string height, double grade, string branch)
+        {
+            this.Name = name;
+            this.Student_id = student_id;
+            this.Birthyear = birthyear;
+            this.Height = height;
+            this.Grade = grade;
+            this.Branch = branch;
+        }
+    }
+}
